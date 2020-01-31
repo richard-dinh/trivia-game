5497ae216298df7659c31b04ab973de64fb92913
Kiara Wu <kiarapwu@gmail.com>
2020-01-31 13:24:49 -0800

    changed jquery link to uncompressed

--- app.js
+++ app.js
@@ -1,9 +1,3 @@
-src="https://code.jquery.com/jquery-3.4.1.js"
-  integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU="
-  crossorigin="anonymous"
-
-
-
 $.get('https://opentdb.com/api_category.php')
 .then((data) => {
     $('#catOptions').textContent = 
@@ -11,9 +5,10 @@ $.get('https://opentdb.com/api_category.php')
   console.log(data.trivia_categories)
   console.log(data.trivia_categories[0].name)
 
-  for(i=0;i>length.trivia_categories;i++)
-  let trivia_categories = [i;]
-})
+//   let categoriesList = data.tr
+//   for(i=0;i>length.trivia_categories;i++)
+//   let trivia_categories = [i;]
+ })
     
 .catch(e=> console.error(e))
 
